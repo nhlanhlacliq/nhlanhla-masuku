951d0bcb0f97f409f8ddf1e71e9872d93276fd7b
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-27 20:23:14 +0200

    change to next

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nhlanhla Masuku",
+  description: "Frontend Developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={cn("min-h-screen", inter.className)}>{children}</body>
+    </html>
+  );
+}

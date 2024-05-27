031d827179dee94084b0677ca94715c463d85082
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-27 23:20:56 +0200

    create layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
+import Nav from "@/components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={cn("min-h-screen", inter.className)}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen lg:flex lg:flex-row bg-gradient-to-bl from-foreground/95 to-foreground",
+          inter.className
+        )}
+      >
+        <Nav />
+        <main className='lg:ml-1/4 lg:w-1/2'>{children}</main>
+      </body>
     </html>
   );
 }

a94457769e6c05b7d5b17b107804853254ab1667
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-28 22:55:15 +0200

    update component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,41 @@
+import Header from "./ui/Header";
+import Section from "./ui/Section";
+import Type from "./ui/Type";
+
 const projects = [
+  {
+    title: "estConnect",
+    description:
+      "Developed an app for estate residents to book amenities, request concierge services, and manage access. Includes an admin dashboard for posting events and managing the estate.",
+    technologies: ["React Native", "Redux", "Node.js", "MongoDB"],
+    link: "#",
+  },
+  {
+    title: "MyPension",
+    description:
+      "Created an app for South Africans to check and withdraw funds available through the government's 2 pot savings system, ensuring easy access to pension details and transactions.",
+    technologies: ["React Native", "GraphQL", "TypeScript"],
+    link: "#",
+  },
+  {
+    title: "Dawao",
+    description:
+      "Built a comprehensive React dashboard for warehouses to track deliveries, manage customers, drivers, and warehouse duties, enhancing operational efficiency.",
+    technologies: ["React", "Redux", "Node.js", "MongoDB"],
+    link: "#",
+  },
   {
     title: "Full-Stack E-commerce Platform",
     description:
-      "Developing a responsive e-commerce platform using JS, React, Next.js, and Sanity.io for aggregating deals mainly in Stellenbosch and across the country.",
+      "Developed a responsive e-commerce platform using JavaScript, React, Next.js, and Sanity.io to aggregate deals, primarily focusing on Stellenbosch and expanding countrywide.",
+    technologies: ["React", "Next.js", "Sanity.io", "JavaScript"],
     link: "#",
   },
   {
     title: "DeltaMax Auto",
     description:
-      "Created an automotive workshop site page using React, TypeScript, and Figma.",
+      "Created a professional site for an automotive workshop using React, TypeScript, and Figma, showcasing services and improving customer engagement.",
+    technologies: ["React", "TypeScript", "Figma"],
     link: "#",
   },
   // Add more projects as needed
@@ -16,31 +43,37 @@ const projects = [
 
 const Projects = () => {
   return (
-    <div id='projects' className={styles.projects}>
-      <h2 className={styles.heading}>Projects</h2>
+    <Section id='projects' style='flex-col'>
+      <Header title='Projects' />
       <div className={styles.grid}>
         {projects.map((project, index) => (
           <div key={index} className={styles.card}>
-            <h3 className={styles.title}>{project.title}</h3>
-            <p className={styles.description}>{project.description}</p>
-            <a href={project.link} className={styles.link}>
-              View Project
-            </a>
+            <Type className={styles.title}>{project.title}</Type>
+            <Type className={styles.description}>{project.description}</Type>
+            <div className={styles.techList}>
+              {project.technologies.map((tech, idx) => (
+                <div key={idx} className={styles.badge}>
+                  <Type className={styles.tech}>{tech}</Type>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
-    </div>
+    </Section>
   );
 };
 
 export default Projects;
 
 const styles = {
-  projects: "bg-white py-16 px-8 lg:px-16",
-  heading: "text-3xl md:text-4xl font-bold text-center mb-12",
-  grid: "grid grid-cols-1 md:grid-cols-2 gap-8",
-  card: "bg-gray-100 p-6 rounded-lg shadow-md",
-  title: "text-xl font-bold mb-2",
-  description: "text-gray-700 mb-4",
-  link: "text-teal-500 hover:underline",
+  grid: "grid grid-cols-1 md:grid-cols-2",
+  card: "my-3",
+  title: "text-l font-bold mb-4",
+  description: "text-sm mb-5 ",
+
+  techList: "flex flex-wrap flex-row gap-2 mb-8",
+  badge:
+    "bg-accent-2-muted rounded-full items-center justify-center items-center px-3 py-1",
+  tech: "mb-0 text-xs text-accent-2 animate-pulse",
 };

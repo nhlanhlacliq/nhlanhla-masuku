c47f135d74f42c76d4e93bf8e9ddb09429c6ac05
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-28 22:54:13 +0200

    add Exp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <About />
+      <Experience />
       <Projects />
     </div>
   );

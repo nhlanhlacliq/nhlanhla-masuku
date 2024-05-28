c790cd483d755e460a3beb29088a621892c3ab2c
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-28 22:54:29 +0200

    rm useEffect

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,65 +1,35 @@
-"use client";
-
-import { useEffect, useRef, useState } from "react";
+import Header from "./ui/Header";
 import Section from "./ui/Section";
+import Type from "./ui/Type";
 
 const About = () => {
-  // const [isSticky, setIsSticky] = useState(false);
-  // const headingRef = useRef(null);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const aboutHeading = headingRef.current;
-  //     if (aboutHeading && window.scrollY > aboutHeading.offsetTop) {
-  //       setIsSticky(true);
-  //     } else if (aboutHeading) {
-  //       setIsSticky(false);
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
-
   return (
     <Section id='about' style='flex-col'>
-      <h2
-        // className={
-        // isSticky
-        // ? styles.heading + " " + styles.stickyHeading
-        // : styles.heading
-        // }
-        className={styles.stickyHeader}
-        id='about-heading'
-      >
-        About
-      </h2>
+      <Header title='About' />
+
       <div className={styles.content}>
-        <p className={styles.text}>
+        <Type className={styles.text}>
           I discovered my love for technology as a kid breaking apart RC cars to
           figure out how they worked. I nurtured this curiosity while working
           with cars at Jaguar Land Rover. From fixing mechanical issues to
           diving into software, I was hooked on the world of innovation and
           problem-solving.
-        </p>
+        </Type>
 
-        <p className={styles.text}>
+        <Type className={styles.text}>
           Every challenge was a chance to learn something new. So, I jumped into
           a Software Engineering Bootcamp to deepen my skills. It was intense,
           but it opened doors to a whole new world of coding and creativity. I
           also dipped my toes into technical writing. Crafting articles and
           documentation for software-focused clients, I found joy in simplifying
           complex concepts for others.
-        </p>
+        </Type>
 
-        <p className={styles.text}>
+        <Type className={styles.text}>
           Now, I'm freelancing, turning ideas into reality for clients. From
           designing user-friendly apps to crafting engaging websites, I'm all
           about making tech work for everyday people.
-        </p>
+        </Type>
       </div>
     </Section>
   );
@@ -68,10 +38,6 @@ const About = () => {
 export default About;
 
 const styles = {
-  heading: "text-3xl md:text-4xl font-bold mb-12 ",
   content: "md:w-3/4",
-  text: "md:text-lg leading-relaxed mb-8",
-  stickyHeading: "sticky top-0 z-10 bg-background/90 bg-opacity-75",
-  stickyHeader:
-    "text-3xl md:text-4xl font-bold mb-12 sticky top-0 backdrop-blur-sm bg-backdrop -mx-8 px-8",
+  text: "opacity-80 mb-5",
 };

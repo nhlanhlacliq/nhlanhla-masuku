5c0d1a60cc746aba32645561bb0cf25c306945e4
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-28 22:55:06 +0200

    update component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,16 @@
 import { Github, Linkedin } from "lucide-react";
 import Section from "./ui/Section";
+import Type from "./ui/Type";
 
 const Hero = () => {
   return (
     <Section id='#' style='flex-col'>
-      <h1 className={styles.title}>Nhlanhla Masuku</h1>
-      <h2 className={styles.subtitle}>Frontend Developer </h2>
-      <h2 className={styles.subtitle2}>Javascript - React / React Native</h2>
-      <p className={styles.text}>
+      <Type variant='title'>Nhlanhla Masuku</Type>
+      <Type variant='subtitle'>Frontend Developer </Type>
+      <Type variant='highlight'>Javascript - React / React Native</Type>
+      <Type className='text-background/75'>
         Mostly Front-end. But, I can Design, Build (Fullstack) and Ship.
-      </p>
+      </Type>
       <div className={styles.iconContainer}>
         <Github size={32} className={styles.icon} />
         <Linkedin size={32} className={styles.icon} />
@@ -21,11 +22,6 @@ const Hero = () => {
 export default Hero;
 
 const styles = {
-  title: "text-4xl md:text-6xl my-4 font-semibold tracking-tight",
-  subtitleContainer: "flex items-center gap-4",
-  subtitle: "text-xl md:text-2xl text-background/90",
-  subtitle2: " mb-8 opacity-75 font-light text-accent-2",
-  text: "mb-8 text-background/75",
   iconContainer: "flex items-center gap-4",
   icon: "rounded-sm p-1 text-background/75",
 };

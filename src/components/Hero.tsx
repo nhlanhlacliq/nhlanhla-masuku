0c3528057d85fbe67c5386fe1e194d00a745cbd5
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-29 22:26:43 +0200

    update ui

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,37 @@
 import { Github, Linkedin } from "lucide-react";
 import Section from "./ui/Section";
 import Type from "./ui/Type";
+import Nav from "./Nav";
 
 const Hero = () => {
   return (
-    <Section id='#' style='flex-col'>
-      <Type variant='title'>Nhlanhla Masuku</Type>
-      <Type variant='subtitle'>Frontend Developer </Type>
-      <Type variant='highlight'>Javascript - React / React Native</Type>
-      <Type className='text-background/75'>
-        Mostly Front-end. But, I can Design, Build (Fullstack) and Ship.
-      </Type>
+    <Section id='#' style={styles.section}>
+      <div>
+        <Type variant='title'>Nhlanhla Masuku</Type>
+        <Type variant='subtitle'>Frontend Developer </Type>
+        <div className='lg:mt-1'>
+          <Type variant='highlight'>Javascript - React / React Native</Type>
+        </div>
+        <Type className='text-background/75'>
+          Mostly Front-end. But, I can Design, Build (Fullstack) and Ship.
+        </Type>
+      </div>
+      <Nav style={styles.nav} />
       <div className={styles.iconContainer}>
-        <Github size={32} className={styles.icon} />
-        <Linkedin size={32} className={styles.icon} />
+        <a
+          href='https://github.com/nhlanhlacliq'
+          target='_blank'
+          className={styles.iconLink}
+        >
+          <Github size={32} className={styles.icon} />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/nhlanhla-masuku'
+          target='_blank'
+          className={styles.iconLink}
+        >
+          <Linkedin size={32} className={styles.icon} />
+        </a>
       </div>
     </Section>
   );
@@ -22,6 +40,9 @@ const Hero = () => {
 export default Hero;
 
 const styles = {
+  section: "flex-col justify-between lg:h-screen lg:fixed",
+  nav: "hidden lg:block text-white",
   iconContainer: "flex items-center gap-4",
-  icon: "rounded-sm p-1 text-background/75",
+  icon: "rounded-sm p-1 text-background/75 hover:text-accent-2 cursor-pointer transition-all duration-300",
+  iconLink: "",
 };

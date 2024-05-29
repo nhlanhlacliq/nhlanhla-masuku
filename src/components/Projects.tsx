a35f48506538e5185946c09860836d2ff5d9bed8
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-29 22:27:11 +0200

    update ui

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,4 @@
+import { MoveRight } from "lucide-react";
 import Header from "./ui/Header";
 import Section from "./ui/Section";
 import Type from "./ui/Type";
@@ -6,37 +7,40 @@ const projects = [
   {
     title: "estConnect",
     description:
-      "Developed an app for estate residents to book amenities, request concierge services, and manage access. Includes an admin dashboard for posting events and managing the estate.",
+      "An app for estate residents to book amenities, request concierge services, and manage access. Includes an admin dashboard for posting events and managing the estate.",
     technologies: ["React Native", "Redux", "Node.js", "MongoDB"],
-    link: "#",
+    link: "",
+    images: [],
   },
   {
     title: "MyPension",
     description:
-      "Created an app for South Africans to check and withdraw funds available through the government's 2 pot savings system, ensuring easy access to pension details and transactions.",
+      "An app for South Africans to check and withdraw funds available through the government's 2 pot savings system, ensuring easy access to pension details and transactions.",
     technologies: ["React Native", "GraphQL", "TypeScript"],
-    link: "#",
+    link: "",
+    images: [],
   },
   {
     title: "Dawao",
     description:
-      "Built a comprehensive React dashboard for warehouses to track deliveries, manage customers, drivers, and warehouse duties, enhancing operational efficiency.",
+      "Comprehensive React dashboard for warehouses to track deliveries, manage customers, drivers, and warehouse duties, enhancing operational efficiency.",
     technologies: ["React", "Redux", "Node.js", "MongoDB"],
-    link: "#",
+    link: "",
+    images: [],
   },
   {
-    title: "Full-Stack E-commerce Platform",
+    title: "DoesDeals",
     description:
-      "Developed a responsive e-commerce platform using JavaScript, React, Next.js, and Sanity.io to aggregate deals, primarily focusing on Stellenbosch and expanding countrywide.",
+      "Responsive e-commerce platform using JavaScript, React, Next.js, and Sanity.io to aggregate deals, primarily focusing on Stellenbosch and expanding countrywide.",
     technologies: ["React", "Next.js", "Sanity.io", "JavaScript"],
-    link: "#",
+    link: "http://doesdeals.co.za/",
   },
   {
     title: "DeltaMax Auto",
     description:
-      "Created a professional site for an automotive workshop using React, TypeScript, and Figma, showcasing services and improving customer engagement.",
+      "A professional site for an automotive workshop using React, TypeScript, and Figma, showcasing services and improving customer engagement.",
     technologies: ["React", "TypeScript", "Figma"],
-    link: "#",
+    link: "https://deltamaxauto.com/",
   },
   // Add more projects as needed
 ];
@@ -48,7 +52,14 @@ const Projects = () => {
       <div className={styles.grid}>
         {projects.map((project, index) => (
           <div key={index} className={styles.card}>
-            <Type className={styles.title}>{project.title}</Type>
+            <a href={project.link} target='_blank'>
+              <div className='flex gap-4 hover:gap-8 transition-all cursor-pointer hover:animate-out'>
+                <Type className={styles.title}>{project.title}</Type>
+                <div className='mt-0.5'>
+                  <MoveRight />
+                </div>
+              </div>
+            </a>
             <Type className={styles.description}>{project.description}</Type>
             <div className={styles.techList}>
               {project.technologies.map((tech, idx) => (
@@ -67,7 +78,7 @@ const Projects = () => {
 export default Projects;
 
 const styles = {
-  grid: "grid grid-cols-1 md:grid-cols-2",
+  grid: "grid grid-cols-1",
   card: "my-3",
   title: "text-l font-bold mb-4",
   description: "text-sm mb-5 ",

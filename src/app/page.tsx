56f5049a2dbbd376e9e0d0c4b470808715e7fc42
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-29 22:26:01 +0200

    add responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
-    <div>
-      <Hero />
-      <About />
-      <Experience />
-      <Projects />
+    <div className='lg:flex'>
+      <div className='lg:w-1/2'>
+        <Hero />
+      </div>
+      <div className='lg:w-1/2 lg:-ml-12 lg:pt-4'>
+        <About />
+        <Experience />
+        <Projects />
+      </div>
     </div>
   );
 }

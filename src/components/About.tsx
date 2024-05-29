ab072f0efc9d6a1499477cbd9736efc3a3358e4f
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-05-29 22:26:13 +0200

    add links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,18 +11,33 @@ const About = () => {
         <Type className={styles.text}>
           I discovered my love for technology as a kid breaking apart RC cars to
           figure out how they worked. I nurtured this curiosity while working
-          with cars at Jaguar Land Rover. From fixing mechanical issues to
-          diving into software, I was hooked on the world of innovation and
-          problem-solving.
+          with cars at{" "}
+          <strong>
+            <a href='https://www.jaguarlandrover.com/' target='_blank'>
+              Jaguar Land Rover
+            </a>
+          </strong>
+          . From fixing mechanical issues to diving into software, I was hooked
+          on the world of innovation and problem-solving.
         </Type>
 
         <Type className={styles.text}>
           Every challenge was a chance to learn something new. So, I jumped into
-          a Software Engineering Bootcamp to deepen my skills. It was intense,
-          but it opened doors to a whole new world of coding and creativity. I
-          also dipped my toes into technical writing. Crafting articles and
-          documentation for software-focused clients, I found joy in simplifying
-          complex concepts for others.
+          a{" "}
+          <strong>
+            <a href='https://www.hyperiondev.com/' target='_blank'>
+              Software Engineering Bootcamp
+            </a>{" "}
+          </strong>
+          to deepen my skills. It was intense, but it opened doors to a whole
+          new world of coding and creativity. I also dipped my toes into{" "}
+          <strong>
+            <a href='https://ritza.co/' target='_blank'>
+              technical writing
+            </a>
+          </strong>
+          . Crafting articles and documentation for software-focused clients, I
+          found joy in simplifying complex concepts for others.
         </Type>
 
         <Type className={styles.text}>
@@ -38,6 +53,6 @@ const About = () => {
 export default About;
 
 const styles = {
-  content: "md:w-3/4",
-  text: "opacity-80 mb-5",
+  content: "",
+  text: "opacity-80 mb-5 text-pretty",
 };
